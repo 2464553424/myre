d8215b20b814de27c1f069c25f71ff1ceedd1182
zhangfeifei <zhangfeifei@anzhiyi.com.cn>
2019-11-30 14:14:56 +0800

    add

---
+++ myres/add/addvc.h
@@ -0,0 +1,17 @@
+//
+//  addvc.h
+//  myres
+//
+//  Created by Fly on 2019/11/30.
+//  Copyright © 2019 Shunde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface addvc : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ myres/add/addvc.m
@@ -0,0 +1,13 @@
+//
+//  addvc.m
+//  myres
+//
+//  Created by Fly on 2019/11/30.
+//  Copyright © 2019 Shunde. All rights reserved.
+//
+
+#import "addvc.h"
+
+@implementation addvc
+
+@end
